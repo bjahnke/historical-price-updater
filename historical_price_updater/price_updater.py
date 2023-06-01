e48e573a84711986edd8109ca8b1544b8c02b6e7
bjahnke <bjahnke71@gmail.com>
2023-06-01 19:45:10 -0400

    added main to price_updater for GCR

--- historical_price_updater/price_updater.py
+++ historical_price_updater/price_updater.py
@@ -5,6 +5,9 @@ import sqlalchemy
 import utils
 import mytypes
 import typing as t
+import socket
+
+from historical_price_updater import env
 
 
 def _download_data(
@@ -163,3 +166,45 @@ def load_pickle_stock_data() -> tuple:
         pathlib.Path("..") / "data" / "bench_data_raw_push_test.pkl"
     )
     return stock_data, bench_data
+
+
+def initiate_data_updater_schedule(
+    download_args: mytypes.DownloadParams,
+    connection_engine: sqlalchemy.Engine,
+    host: str,
+    port: int,
+) -> None:
+    """
+    Schedule the script to run once a day at a specific time.
+
+    :param host:
+    :param port:
+    :param connection_engine:
+    :param download_args:
+    :param trigger_time: The time to run the script.
+    """
+    # Create a socket object
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        # Bind the socket to a specific address and port
+        s.bind((host, port))
+
+        # Listen for incoming connections
+        s.listen()
+
+        task_save_historical_data_to_database(
+            download_args=download_args, connection_engine=connection_engine
+        )
+
+
+if __name__ == '__main__':
+    initiate_data_updater_schedule(
+        download_args=mytypes.DownloadParams(
+            stock_table_url=env.DOWNLOAD_STOCK_TABLE_URL,
+            bench=env.DOWNLOAD_BENCHMARK_SYMBOL,
+            days=env.DOWNLOAD_DAYS_BACK,
+            interval_str=env.DOWNLOAD_DATA_INTERVAL,
+        ),
+        connection_engine=env.ConnectionEngines.HistoricalPrices.NEON,
+        host=env.HOST,
+        port=env.PORT
+    )
